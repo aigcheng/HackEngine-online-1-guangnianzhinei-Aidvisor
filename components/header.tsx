53709385e6f9bb5d35024490e0138291efe50f08
czen <czen@192.168.1.5>
2023-04-09 14:20:14 +0800

    feat: 联系人页面

--- components/header.tsx
+++ components/header.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import Link from 'next/link'
 import styles from '@/styles/Home.module.css'
-export default function Header() {
+export default function Header(props: any) {
   const [showModal, setShowModal] = useState(false)
   const [phone, setPhone] = useState('')
   const [password, setPassword] = useState('')
@@ -26,80 +26,84 @@ export default function Header() {
     <header className="sticky top-0 z-40 w-full h-20 bg-white">
       <div className="flex h-16 items-center space-x-4 mx-20">
         <div className="flex gap-6 md:gap-10">
-          <svg viewBox="0 0 34 16" fill="none" xmlns="http://www.w3.org/2000/svg" className={styles.nav_eyes}>
-            <g
-              id="eyeLeft"
-              data-svg-origin="8 8"
-              transform="matrix(-0.80326,0.59562,-0.59562,-0.80326,19.19104,9.66112)"
-              style={{ transformOrigin: '0px 0px;' }}
-            >
-              <path
-                d="M8 15.25C12.0041 15.25 15.25 12.0041 15.25 8C15.25 3.99594 12.0041 0.75 8 0.75C3.99594 0.75 0.75 3.99594 0.75 8C0.75 12.0041 3.99594 15.25 8 15.25Z"
-                stroke="currentColor"
-                strokeWidth="1.5"
-              ></path>
-              <path
-                d="M8 9C10.2091 9 12 7.20914 12 5C12 2.79086 10.2091 1 8 1C5.79086 1 4 2.79086 4 5C4 7.20914 5.79086 9 8 9Z"
-                fill="currentColor"
-              ></path>
-            </g>
-            <g
-              id="eyeRight"
-              data-svg-origin="26 8"
-              transform="matrix(-0.80326,0.59562,-0.59562,-0.80326,51.64972,-1.06004)"
-              style={{ transformOrigin: '0px 0px;' }}
-            >
-              <path
-                d="M26 15.25C30.0041 15.25 33.25 12.0041 33.25 8C33.25 3.99594 30.0041 0.75 26 0.75C21.9959 0.75 18.75 3.99594 18.75 8C18.75 12.0041 21.9959 15.25 26 15.25Z"
-                stroke="currentColor"
-                strokeWidth="1.5"
-              ></path>
-              <path
-                d="M26 9C28.2091 9 30 7.20914 30 5C30 2.79086 28.2091 1 26 1C23.7909 1 22 2.79086 22 5C22 7.20914 23.7909 9 26 9Z"
-                fill="currentColor"
-              ></path>
-            </g>
-          </svg>
+          <Link href="/">
+            <svg viewBox="0 0 34 16" fill="none" xmlns="http://www.w3.org/2000/svg" className={styles.nav_eyes}>
+              <g
+                id="eyeLeft"
+                data-svg-origin="8 8"
+                transform="matrix(-0.80326,0.59562,-0.59562,-0.80326,19.19104,9.66112)"
+                style={{ transformOrigin: '0px 0px;' }}
+              >
+                <path
+                  d="M8 15.25C12.0041 15.25 15.25 12.0041 15.25 8C15.25 3.99594 12.0041 0.75 8 0.75C3.99594 0.75 0.75 3.99594 0.75 8C0.75 12.0041 3.99594 15.25 8 15.25Z"
+                  stroke="currentColor"
+                  strokeWidth="1.5"
+                ></path>
+                <path
+                  d="M8 9C10.2091 9 12 7.20914 12 5C12 2.79086 10.2091 1 8 1C5.79086 1 4 2.79086 4 5C4 7.20914 5.79086 9 8 9Z"
+                  fill="currentColor"
+                ></path>
+              </g>
+              <g
+                id="eyeRight"
+                data-svg-origin="26 8"
+                transform="matrix(-0.80326,0.59562,-0.59562,-0.80326,51.64972,-1.06004)"
+                style={{ transformOrigin: '0px 0px;' }}
+              >
+                <path
+                  d="M26 15.25C30.0041 15.25 33.25 12.0041 33.25 8C33.25 3.99594 30.0041 0.75 26 0.75C21.9959 0.75 18.75 3.99594 18.75 8C18.75 12.0041 21.9959 15.25 26 15.25Z"
+                  stroke="currentColor"
+                  strokeWidth="1.5"
+                ></path>
+                <path
+                  d="M26 9C28.2091 9 30 7.20914 30 5C30 2.79086 28.2091 1 26 1C23.7909 1 22 2.79086 22 5C22 7.20914 23.7909 9 26 9Z"
+                  fill="currentColor"
+                ></path>
+              </g>
+            </svg>
+          </Link>
           <h2 className="font-inter font-bold text-2xl leading-7 text-black flex items-center">
             CHANPIN
             <span className="text-blue-500">MING</span>
           </h2>
         </div>
-        <div className="flex flex-1 items-center justify-end space-x-4">
-          <nav className="flex items-center space-x-1 ">
-            <ul className=" gap-10 font-display text-[15px] font-medium text-secondary-700 md:flex header-menu w-[50vw]">
-              <li>
-                <Link
-                  href="/contacts"
-                  className="relative cursor-pointer font-inter font-normal text-black text-2xl leading-6 flex items-center justify-center"
-                >
-                  联系人
-                  <small className="ml-1 rounded bg-primary-500 px-1 py-0.5 text-xs font-medium text-white">NEW</small>
-                </Link>
-              </li>
-              <li>
-                <Link href="/recording" className="font-inter font-normal text-black text-2xl leading-6 flex items-center justify-center">
-                  日程
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="/member-info"
-                  target="_blank"
-                  className="font-inter font-normal text-black text-2xl leading-6 flex items-center justify-center"
-                >
-                  聊天
-                </Link>
-              </li>
-            </ul>
-            <button
-              onClick={handleOpenModal}
-              className="bg-blue-500 rounded-full md:min-w-[110px] border border-primary-500 bg-primary-500 px-3 py-1.5 md:px-4 md:py-2 text-center text-sm font-medium text-white shadow-sm transition-all hover:border-primary-700 hover:bg-primary-700 focus:ring focus:ring-primary-200"
-            >
-              开始使用
-            </button>
-          </nav>
-        </div>
+        {props.showNav && (
+          <div className="flex flex-1 items-center justify-end space-x-4">
+            <nav className="flex items-center space-x-1 ">
+              <ul className=" gap-10 font-display text-[15px] font-medium text-secondary-700 md:flex header-menu w-[50vw]">
+                <li>
+                  <Link
+                    href="/contacts"
+                    className="relative cursor-pointer font-inter font-normal text-black text-2xl leading-6 flex items-center justify-center"
+                  >
+                    联系人
+                    <small className="ml-1 rounded bg-primary-500 px-1 py-0.5 text-xs font-medium text-white">NEW</small>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/recording" className="font-inter font-normal text-black text-2xl leading-6 flex items-center justify-center">
+                    日程
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/member-info"
+                    target="_blank"
+                    className="font-inter font-normal text-black text-2xl leading-6 flex items-center justify-center"
+                  >
+                    聊天
+                  </Link>
+                </li>
+              </ul>
+              <button
+                onClick={handleOpenModal}
+                className="bg-blue-500 rounded-full md:min-w-[110px] border border-primary-500 bg-primary-500 px-3 py-1.5 md:px-4 md:py-2 text-center text-sm font-medium text-white shadow-sm transition-all hover:border-primary-700 hover:bg-primary-700 focus:ring focus:ring-primary-200"
+              >
+                开始使用
+              </button>
+            </nav>
+          </div>
+        )}
       </div>
 
       {/* 弹窗 */}
--- pages/contacts.tsx
+++ pages/contacts.tsx
@@ -1,44 +1,181 @@
-import { Avatar, List } from 'antd';
+import { Avatar, List, Layout, Input, Button } from 'antd'
+import { useState } from 'react'
+import Header from '@/components/header'
+const { Content } = Layout
 
-const contacts = [
+interface Contact {
+  id: number
+  name: string
+  phone: string
+  company: string
+  industry: string
+  position: string
+  birthday: string
+  soundImpression: string
+  avatar: string
+}
+
+const contacts: Contact[] = [
   {
     id: 1,
     name: '张三',
-    phone: '13512345678',
-    avatar: 'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_960_720.png',
+    phone: '13812345678',
+    company: 'ABC公司',
+    industry: 'IT',
+    position: '工程师',
+    birthday: '1990年1月1日',
+    soundImpression: '说话很温柔',
+    avatar: 'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_960_720.png'
   },
   {
     id: 2,
     name: '李四',
-    phone: '13612345678',
-    avatar: 'https://cdn.pixabay.com/photo/2016/03/31/20/27/avatar-1295773_960_720.png',
+    phone: '13987654321',
+    company: 'XYZ公司',
+    industry: '金融',
+    position: '经理',
+    birthday: '1985年5月5日',
+    soundImpression: '声音很有磁性',
+    avatar: 'https://cdn.pixabay.com/photo/2016/03/31/20/27/avatar-1295773_960_720.png'
   },
   {
     id: 3,
     name: '王五',
-    phone: '13712345678',
-    avatar: 'https://cdn.pixabay.com/photo/2016/04/01/11/25/avatar-1300331_960_720.png',
-  },
-];
+    phone: '13987654321',
+    company: 'XYZ公司',
+    industry: '互联网',
+    position: '经理',
+    birthday: '1998年5月5日',
+    soundImpression: '声音很有特点',
+    avatar: 'https://cdn.pixabay.com/photo/2016/04/01/11/25/avatar-1300331_960_720.png'
+  }
+]
 
 function ContactsPage() {
-  return (
-    <div>
-      <List
-        itemLayout="horizontal"
-        dataSource={contacts}
-        renderItem={(contact) => (
-          <List.Item>
-            <List.Item.Meta
-              avatar={<Avatar src={contact.avatar} />}
-              title={contact.name}
-              description={contact.phone}
-            />
-          </List.Item>
-        )}
+  const [selectedContact, setSelectedContact] = useState<Contact | null>(contacts[0])
+  const [editableField, setEditableField] = useState('')
+
+  const handleContactSelect = (contact: Contact) => {
+    setSelectedContact(contact)
+  }
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (selectedContact) {
+      setSelectedContact({
+        ...selectedContact,
+        [editableField]: event.target.value
+      })
+    }
+  }
+
+  const handleSaveClick = () => {
+    // 将修改后的联系人信息保存到数据库或其他状态管理器中
+    console.log('Saved:', selectedContact)
+    setEditableField('')
+  }
+
+  const getFieldEditor = (field: keyof Contact) => {
+    return (
+      <Input
+        value={selectedContact ? selectedContact[field] : ''}
+        onChange={handleInputChange}
+        onPressEnter={handleSaveClick}
+        onBlur={() => setEditableField('')}
+        autoFocus
       />
-    </div>
-  );
+    )
+  }
+
+  return (
+    <Layout>
+      <Header></Header>
+      <Content className="flex">
+        <div className="w-1/5 h-screen bg-blue-200 bg-opacity-15">
+          <List
+            itemLayout="horizontal"
+            dataSource={contacts}
+            renderItem={contact => (
+              <List.Item
+                key={contact.id}
+                onClick={() => handleContactSelect(contact)}
+                className={`p-4 cursor-pointer ${selectedContact?.id === contact.id ? 'bg-gray-200' : ''}`}
+              >
+                <List.Item.Meta
+                  className="mx-4"
+                  avatar={<Avatar src={contact.avatar} />}
+                  title={contact.name}
+                  description={contact.phone}
+                />
+              </List.Item>
+            )}
+          />
+        </div>
+        {selectedContact && (
+          <div className="w-4/5 h-screen bg-gray-100 text-black flex flex-col">
+            <div className="flex flex-col items-center justify-around py-6">
+              <Avatar src={selectedContact.avatar} size={96} />
+              <h2 className="text-2xl font-bold">{selectedContact.name}</h2>
+            </div>
+            <div className="grid grid-cols-2 gap-8 px-12">
+              <div className="flex">
+                <div className="font-bold mb-4">公司：</div>
+                <div>
+                  {editableField === 'company' ? (
+                    getFieldEditor('company')
+                  ) : (
+                    <span onClick={() => setEditableField('company')} className="cursor-pointer">
+                      {selectedContact.company}
+                    </span>
+                  )}
+                </div>
+              </div>
+              <div className="flex">
+                <div className="font-bold mb-4">行业：</div>
+                <div>
+                  {editableField === 'industry' ? (
+                    getFieldEditor('industry')
+                  ) : (
+                    <span onClick={() => setEditableField('industry')} className="cursor-pointer">
+                      {selectedContact.industry}
+                    </span>
+                  )}
+                </div>
+              </div>
+              <div className="flex">
+                <div className="font-bold mb-4">职位：</div>
+                <div>
+                  {editableField === 'position' ? (
+                    getFieldEditor('position')
+                  ) : (
+                    <span onClick={() => setEditableField('position')} className="cursor-pointer">
+                      {selectedContact.position}
+                    </span>
+                  )}
+                </div>
+              </div>
+              <div className="flex">
+                <div className="font-bold mb-4">生日：</div>
+                <div>
+                  {editableField === 'birthday' ? (
+                    getFieldEditor('birthday')
+                  ) : (
+                    <span onClick={() => setEditableField('birthday')} className="cursor-pointer">
+                      {selectedContact.birthday}
+                    </span>
+                  )}
+                </div>
+              </div>
+            </div>
+            <div className="flex justify-center my-8">
+              <Button onClick={handleSaveClick} className=" w-60 h-10 bg-blue rounded-full">
+                保存
+              </Button>
+            </div>
+          </div>
+        )}
+      </Content>
+    </Layout>
+  )
 }
 
-export default ContactsPage;
+export default ContactsPage
