bf74a2bd5d4451458ecb54eeec794dd553866464
czen <czen@192.168.1.5>
2023-04-09 14:51:48 +0800

    fix: ts

--- pages/member-info.tsx
+++ pages/member-info.tsx
@@ -1,36 +1,77 @@
-import { Avatar, Button, Modal } from 'antd';
-import { useState } from 'react';
+import { Avatar, Button, Modal } from 'antd'
+import { JSXElementConstructor, ReactElement, ReactFragment, ReactPortal, useState } from 'react'
 
-function MemberInfoPage(props) {
-  const [isModalVisible, setIsModalVisible] = useState(false);
+function MemberInfoPage(props: {
+  member: {
+    avatar:
+      | string
+      | number
+      | boolean
+      | ReactElement<any, string | JSXElementConstructor<any>>
+      | ReactFragment
+      | ReactPortal
+      | null
+      | undefined
+    name:
+      | string
+      | number
+      | boolean
+      | ReactElement<any, string | JSXElementConstructor<any>>
+      | ReactFragment
+      | ReactPortal
+      | null
+      | undefined
+    phone:
+      | string
+      | number
+      | boolean
+      | ReactElement<any, string | JSXElementConstructor<any>>
+      | ReactFragment
+      | ReactPortal
+      | null
+      | undefined
+  }
+}) {
+  const [isModalVisible, setIsModalVisible] = useState(false)
 
   // 处理点击添加印象按钮
   const handleAddImpressionClick = () => {
-    setIsModalVisible(true);
-  };
+    setIsModalVisible(true)
+  }
 
   // 处理模态框关闭
   const handleModalClose = () => {
-    setIsModalVisible(false);
-  };
+    setIsModalVisible(false)
+  }
 
   return (
     <div>
-      <Avatar src={props.member.avatar} size={64} />
+      {/* <Avatar src={props.member.avatar} size={64} />
       <p>姓名：{props.member.name}</p>
       <p>电话号码：{props.member.phone}</p>
-      <Button type="primary" onClick={handleAddImpressionClick}>添加印象</Button>
+      <Button type="primary" onClick={handleAddImpressionClick}>
+        添加印象
+      </Button> */}
 
       {/* 添加印象模态框 */}
-      <Modal title="添加印象" visible={isModalVisible} onCancel={handleModalClose} footer={[
-        <Button key="cancel" onClick={handleModalClose}>取消</Button>,
-        <Button key="submit" type="primary" onClick={handleModalClose}>提交</Button>,
-      ]}>
+      {/* <Modal
+        title="添加印象"
+        visible={isModalVisible}
+        onCancel={handleModalClose}
+        footer={[
+          <Button key="cancel" onClick={handleModalClose}>
+            取消
+          </Button>,
+          <Button key="submit" type="primary" onClick={handleModalClose}>
+            提交
+          </Button>
+        ]}
+      >
         <p>请说出您的印象：</p>
         <textarea rows="4" cols="50"></textarea>
-      </Modal>
+      </Modal> */}
     </div>
-  );
+  )
 }
 
-export default MemberInfoPage;
+export default MemberInfoPage
